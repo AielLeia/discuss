f77ca6bae723e36dcb238d234a41d5a66be2c1e5
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-02-11 17:35:41 +0100

    Refactor Header and Home components to benefit pre rendered as static content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-import { auth } from '@/auth';
-import { Button } from '@nextui-org/react';
-
-import Profile from '@/components/profile';
-
-import * as actions from '@/actions';
-
-export default async function Home() {
-  const session = await auth();
-
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign out</Button>
-      </form>
-
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div>Home Page</div>;
 }
